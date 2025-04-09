c9b8f987e33fb323d878598aaa7518b5805e6568
Acekaaaa <achraf.kndsi@gmail.com>
2025-04-10 00:59:46 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import ACEApp from './main';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ACEApp from "./main";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><ACEApp /></React.StrictMode>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ACEApp />
+  </React.StrictMode>
+);
